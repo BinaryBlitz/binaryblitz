ae7fcb504d3c24a5b275da7b716cde3813f01216
Dan Kim <itsdanya@gmail.com>
2022-05-08 19:15:45 +0300

    Clean up default layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,11 +1,9 @@
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
+    <div>
       <HelloWorld :msg="$frontmatter.title" />
 
-      <div id="nav">
+      <div>
         <router-link to="/">Home</router-link>
         <router-link to="/about">About</router-link>
       </div>
