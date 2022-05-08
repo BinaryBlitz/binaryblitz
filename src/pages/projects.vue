33e9a96e86ffc484dfb8eb62fefe5c694d563fa8
Dan Kim <itsdanya@gmail.com>
2022-05-09 02:21:07 +0300

    useDocuments

--- src/pages/projects.vue
+++ src/pages/projects.vue
@@ -1,6 +1,21 @@
+<script setup lang="ts">
+const projects = useDocuments('~/pages/projects')
+</script>
+
 <template>
   <div class="bg-gray-50">
     <div class="container py-12 grid grid-cols-2">
+
+      <router-link :to="`/projects/${project.slug}`"
+        class="bg-cover p-12 flex flex-col justify-between items-start gap-y-20" v-for="project in projects"
+        :style="`background-image: url(${project.background_url})`">
+        <img alt="" :src="project.icon_src" class="h-12">
+
+        <div class="font-medium text-white">
+          <p class="text-sm">{{ project.category }}</p>
+          <p class="text-lg font-semibold">{{ project.headline }}</p>
+        </div>
+      </router-link>
 
       <router-link to="/projects/fitmost" class="bg-cover p-12 flex flex-col justify-between items-start gap-y-20"
         style="background-image: url('/images/works-bg0.jpg')">
