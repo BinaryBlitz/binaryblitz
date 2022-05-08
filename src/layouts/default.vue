5b9f04773596a1eeae0513bb4709f7f4df1c1d44
Dan Kim <itsdanya@gmail.com>
2022-05-08 19:56:02 +0300

    Clean up default layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,15 +1,5 @@
 <template>
   <Header />
-  <header>
-    <div>
-      <HelloWorld :msg="$frontmatter.title" />
-
-      <div>
-        <router-link to="/">Home</router-link>
-        <router-link to="/about">About</router-link>
-      </div>
-    </div>
-  </header>
 
   <slot />
 </template>
---
+++ src/pages/index.vue
@@ -0,0 +1,11 @@
+<page>
+title: You did it!
+</page>
+
+<template>
+  <div class="bg-gray-100 py-12">
+    <div class="container mx-auto">
+      kek
+    </div>
+  </div>
+</template>
