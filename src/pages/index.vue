ddd39b3a22959a705ebb356c1382f5dc55145b97
Dan Kim <itsdanya@gmail.com>
2022-05-08 20:56:17 +0300

    Add 'Mobile apps' to index

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,9 +3,29 @@ title: You did it!
 </page>
 
 <template>
-  <div class="bg-gray-100 py-12">
-    <div class="container mx-auto">
-      kek
+  <div class="bg-gray-100">
+    <div class="container mx-auto py-12 grid">
+
+      <section class="grid grid-cols-12 gap-x-16">
+        <div class="col-span-6 flex flex-col gap-y-8 justify-center mb-16">
+          <h2 class="text-2xl font-medium">
+            Mobile apps
+          </h2>
+          <p class="text-lg">
+            We use modern technologies and innovative design solutions when developing our apps. The users will love
+            your product.
+          </p>
+          <div>
+            <a href="#" class="button">
+              Our projects
+            </a>
+          </div>
+        </div>
+        <div class="col-span-6 flex items-center justify-center">
+          <img src="@/assets/images/iPhone-screen.png" />
+        </div>
+      </section>
+
     </div>
   </div>
 </template>
