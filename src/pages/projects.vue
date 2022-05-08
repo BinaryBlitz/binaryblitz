a76febf2d19c78c157b93d0e2b536a00da435bfd
Dan Kim <itsdanya@gmail.com>
2022-05-09 00:53:41 +0300

    Add links to projects

--- src/pages/projects.vue
+++ src/pages/projects.vue
@@ -2,7 +2,7 @@
   <div class="bg-gray-50">
     <div class="container py-12 grid grid-cols-2">
 
-      <a href="#" class="bg-cover p-12 flex flex-col justify-between items-start gap-y-20"
+      <router-link to="/projects/fitmost" class="bg-cover p-12 flex flex-col justify-between items-start gap-y-20"
         style="background-image: url('/images/works-bg0.jpg')">
         <img alt="" src="@/assets/images/projects/fitmost/logo-round.png" class="h-12">
 
@@ -10,8 +10,8 @@
           <p class="text-sm">Mobile app</p>
           <p class="text-lg font-semibold">Universal fitness pass</p>
         </div>
-      </a>
-      <a href="#" class="bg-cover p-12 flex flex-col justify-between items-start gap-y-20"
+      </router-link>
+      <router-link to="/projects/fitmost" class="bg-cover p-12 flex flex-col justify-between items-start gap-y-20"
         style="background-image: url('/images/works-bg3.jpg')">
         <img alt="" src="@/assets/images/works-logo3.png" class="h-12">
 
@@ -19,8 +19,8 @@
           <p class="text-sm">Mobile app</p>
           <p class="text-lg font-semibold">Dry-cleaning delivered</p>
         </div>
-      </a>
-      <a href="#" class="bg-cover p-12 flex flex-col justify-between items-start gap-y-20"
+      </router-link>
+      <router-link to="/projects/fitmost" class="bg-cover p-12 flex flex-col justify-between items-start gap-y-20"
         style="background-image: url('/images/works-bg5.jpg')">
         <img alt="" src="@/assets/images/works-logo5.png" class="h-12">
 
@@ -28,8 +28,8 @@
           <p class="text-sm">Mobile app</p>
           <p class="text-lg font-semibold">Diet food delivery</p>
         </div>
-      </a>
-      <a href="#" class="bg-cover p-12 flex flex-col justify-between items-start gap-y-20"
+      </router-link>
+      <router-link to="/projects/fitmost" class="bg-cover p-12 flex flex-col justify-between items-start gap-y-20"
         style="background-image: url('/images/works-bg6.jpg')">
         <img alt="" src="@/assets/images/works-logo6.png" class="h-12">
 
@@ -37,8 +37,8 @@
           <p class="text-sm">Mobile app</p>
           <p class="text-lg font-semibold">Chain of city cafes</p>
         </div>
-      </a>
-      <a href="#" class="bg-cover p-12 flex flex-col justify-between items-start gap-y-20"
+      </router-link>
+      <router-link to="/projects/fitmost" class="bg-cover p-12 flex flex-col justify-between items-start gap-y-20"
         style="background-image: url('/images/works-bg7.jpg')">
         <img alt="" src="@/assets/images/works-logo7.png" class="h-12">
 
@@ -46,8 +46,8 @@
           <p class="text-sm">Mobile app</p>
           <p class="text-lg font-semibold">Trivia game</p>
         </div>
-      </a>
-      <a href="#" class="bg-cover p-12 flex flex-col justify-between items-start gap-y-20"
+      </router-link>
+      <router-link to="/projects/fitmost" class="bg-cover p-12 flex flex-col justify-between items-start gap-y-20"
         style="background-image: url('/images/works-bg2.jpg')">
         <img alt="" src="@/assets/images/works-logo2.png" class="h-12">
 
@@ -55,8 +55,8 @@
           <p class="text-sm">Mobile app</p>
           <p class="text-lg font-semibold">Study assistant</p>
         </div>
-      </a>
-      <a href="#" class="bg-cover p-12 flex flex-col justify-between items-start gap-y-20"
+      </router-link>
+      <router-link to="/projects/fitmost" class="bg-cover p-12 flex flex-col justify-between items-start gap-y-20"
         style="background-image: url('/images/works-bg1.jpg')">
         <img alt="" src="@/assets/images/works-logo1.png" class="h-12">
 
@@ -64,8 +64,7 @@
           <p class="text-sm">Mobile app</p>
           <p class="text-lg font-semibold">Search for sporting events</p>
         </div>
-      </a>
-
+      </router-link>
 
     </div>
   </div>
