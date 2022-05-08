83637c4b2cb5c821c60bc8db3215496fe8a5a323
Dan Kim <itsdanya@gmail.com>
2022-05-08 19:55:17 +0300

    Add consulting page

---
+++ src/pages/consulting.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="bg-gray-100 py-12">
+    <div class="container mx-auto flex flex-col gap-y-8">
+      <h2 class="text-2xl font-medium">Increase conversion rate of your app or website</h2>
+
+      <p>
+        We can help even if you already have an app or a website. Our team will analyze your product. We will offer
+        improvements to your product to make it modern and more efficient.
+      </p>
+
+      <p>
+        Conversion is the key metric. The ratio of users who performed desired action (clicks, purchases, signups,
+        reviews,
+        etc.) to the total number of users.
+      </p>
+
+      <p>
+        By using statistics collected by analytic tools (e.g. Google Analytics, AppsFlyer, etc.) our experts can analyze
+        your revenue sources. After that, our team will recommend content, design and usability improvements to your
+        product
+        to improve your key indicators.
+      </p>
+    </div>
+  </div>
+</template>
