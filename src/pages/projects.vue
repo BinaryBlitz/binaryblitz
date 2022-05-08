459a28ecfb41c959a1971ee9fd44c66ae396d55a
Dan Kim <itsdanya@gmail.com>
2022-05-08 22:35:33 +0300

    Add projects

---
+++ src/pages/projects.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="bg-gray-50">
+    <div class="container mx-auto py-12 grid grid-cols-2">
+
+      <a href="#" class="bg-cover p-12 flex flex-col justify-between items-start gap-y-20"
+        style="background-image: url('/images/works-bg0.jpg')">
+        <img alt="" src="@/assets/images/projects/fitmost/logo-round.png" class="h-12">
+
+        <div class="font-medium text-white">
+          <p class="text-sm">Mobile app</p>
+          <p class="text-lg">Universal fitness pass</p>
+        </div>
+      </a>
+      <a href="#" class="bg-cover p-12 flex flex-col justify-between items-start gap-y-20"
+        style="background-image: url('/images/works-bg3.jpg')">
+        <img alt="" src="@/assets/images/works-logo3.png" class="h-12">
+
+        <div class="font-medium text-white">
+          <p class="text-sm">Mobile app</p>
+          <p class="text-lg">Dry-cleaning delivered</p>
+        </div>
+      </a>
+      <a href="#" class="bg-cover p-12 flex flex-col justify-between items-start gap-y-20"
+        style="background-image: url('/images/works-bg5.jpg')">
+        <img alt="" src="@/assets/images/works-logo5.png" class="h-12">
+
+        <div class="font-medium text-gray-900">
+          <p class="text-sm">Mobile app</p>
+          <p class="text-lg">Diet food delivery</p>
+        </div>
+      </a>
+      <a href="#" class="bg-cover p-12 flex flex-col justify-between items-start gap-y-20"
+        style="background-image: url('/images/works-bg6.jpg')">
+        <img alt="" src="@/assets/images/works-logo6.png" class="h-12">
+
+        <div class="font-medium text-gray-900">
+          <p class="text-sm">Mobile app</p>
+          <p class="text-lg">Chain of city cafes</p>
+        </div>
+      </a>
+      <a href="#" class="bg-cover p-12 flex flex-col justify-between items-start gap-y-20"
+        style="background-image: url('/images/works-bg7.jpg')">
+        <img alt="" src="@/assets/images/works-logo7.png" class="h-12">
+
+        <div class="font-medium text-white">
+          <p class="text-sm">Mobile app</p>
+          <p class="text-lg">Trivia game</p>
+        </div>
+      </a>
+      <a href="#" class="bg-cover p-12 flex flex-col justify-between items-start gap-y-20"
+        style="background-image: url('/images/works-bg2.jpg')">
+        <img alt="" src="@/assets/images/works-logo2.png" class="h-12">
+
+        <div class="font-medium text-white">
+          <p class="text-sm">Mobile app</p>
+          <p class="text-lg">Study assistant</p>
+        </div>
+      </a>
+      <a href="#" class="bg-cover p-12 flex flex-col justify-between items-start gap-y-20"
+        style="background-image: url('/images/works-bg1.jpg')">
+        <img alt="" src="@/assets/images/works-logo1.png" class="h-12">
+
+        <div class="font-medium text-gray-900">
+          <p class="text-sm">Mobile app</p>
+          <p class="text-lg">Search for sporting events</p>
+        </div>
+      </a>
+
+
+    </div>
+  </div>
+</template>
