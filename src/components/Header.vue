730e069f3b73dade7e0478f8bb7eb0e6747ad434
Dan Kim <itsdanya@gmail.com>
2022-05-08 19:38:48 +0300

    Header: add layout

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="container mx-auto py-4">
-    Hello
+  <div class="container mx-auto py-8 flex justify-between">
+    <div class="flex flex-row items-center gap-x-12">
+      <a href="/">
+        <img src="@/assets/images/logo.png" alt="Binary Blitz logo">
+      </a>
+      <ul class="flex gap-8">
+        <li>Projects</li>
+        <li>Careers</li>
+        <li>Consulting</li>
+      </ul>
+    </div>
+
+    <div class="flex gap-x-8">
+      <a href="#">EN</a>
+      <a href="#">Contact</a>
+    </div>
   </div>
 </template>
