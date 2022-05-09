98285b060baa2aacb51b6a8441a8c1029a90670a
Dan Kim <itsdanya@gmail.com>
2022-05-09 22:17:15 +0300

    Update consulting

--- src/pages/consulting.vue
+++ src/pages/consulting.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-gray-100">
     <div class="container flex flex-col gap-y-8 py-12">
-      <h2 class="text-2xl font-medium">Increase conversion rate of your app or website</h2>
+      <h2 class="text-3xl font-semibold">Increase conversion rate of your app or website</h2>
 
       <p>
         We can help even if you already have an app or a website. Our team will analyze your product. We will offer
@@ -20,6 +20,50 @@
         product
         to improve your key indicators.
       </p>
+    </div>
+  </div>
+  <div class="container py-12">
+    <h2 class="text-3xl font-semibold text-center mb-12">How we improve conversion rate</h2>
+
+    <div class="flex flex-wrap gap-8">
+      <div class="flex flex-col items-start gap-y-2 flex-1">
+        <img src="@/assets/images/conversion1.png" alt="Conversion step 1">
+        <h3 class="pl-10 text-lg font-bold">Setting up analytics</h3>
+        <p class="pl-10">
+          Collecting statistics about your current conversion rate.
+        </p>
+      </div>
+      <div class="flex flex-col items-start gap-y-2 flex-1">
+        <img src="@/assets/images/conversion2.png" alt="Conversion step 1">
+        <h3 class="pl-10 text-lg font-bold">Analyzing ads</h3>
+        <p class="pl-10">
+          Recommendations to improve your ad campaigns.
+        </p>
+      </div>
+      <div class="flex flex-col items-start gap-y-2 flex-1">
+        <img src="@/assets/images/conversion3.png" alt="Conversion step 1">
+        <h3 class="pl-10 text-lg font-bold">Usability audit</h3>
+        <p class="pl-10">
+          We will audit your source code, design, site logic. Recommendations for many aspects of your product.
+        </p>
+      </div>
+
+      <div class="basis-full h-0"></div>
+
+      <div class="flex flex-col items-start gap-y-2 flex-1">
+        <img src="@/assets/images/conversion4.png" alt="Conversion step 1">
+        <h3 class="pl-10 text-lg font-bold">Making changes</h3>
+        <p class="pl-10">
+          All recommended changes can be implemented by our team.
+        </p>
+      </div>
+      <div class="flex flex-col items-start gap-y-2 flex-1">
+        <img src="@/assets/images/conversion5.png" alt="Conversion step 1">
+        <h3 class="pl-10 text-lg font-bold">Result</h3>
+        <p class="pl-10">
+          We will provide the complete report containing data on improvements to your metrics.
+        </p>
+      </div>
     </div>
   </div>
 </template>
