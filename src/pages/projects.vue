6c23029ffbc55482423f7578a0b19ec2850447cc
Dan Kim <itsdanya@gmail.com>
2022-05-09 21:07:08 +0300

    Clean up projects

--- src/pages/projects.vue
+++ src/pages/projects.vue
@@ -17,34 +17,6 @@ const projects = useDocuments('~/pages/projects')
         </div>
       </router-link>
 
-      <router-link to="/projects/fitmost" class="bg-cover p-12 flex flex-col justify-between items-start gap-y-20"
-        style="background-image: url('/images/works-bg7.jpg')">
-        <img alt="" src="/assets/images/works-logo7.png" class="h-12">
-
-        <div class="font-medium text-white">
-          <p class="text-sm">Mobile app</p>
-          <p class="text-lg font-semibold">Trivia game</p>
-        </div>
-      </router-link>
-      <router-link to="/projects/fitmost" class="bg-cover p-12 flex flex-col justify-between items-start gap-y-20"
-        style="background-image: url('/images/works-bg2.jpg')">
-        <img alt="" src="/assets/images/works-logo2.png" class="h-12">
-
-        <div class="font-medium text-white">
-          <p class="text-sm">Mobile app</p>
-          <p class="text-lg font-semibold">Study assistant</p>
-        </div>
-      </router-link>
-      <router-link to="/projects/fitmost" class="bg-cover p-12 flex flex-col justify-between items-start gap-y-20"
-        style="background-image: url('/images/works-bg1.jpg')">
-        <img alt="" src="/assets/images/works-logo1.png" class="h-12">
-
-        <div class="font-medium text-gray-900">
-          <p class="text-sm">Mobile app</p>
-          <p class="text-lg font-semibold">Search for sporting events</p>
-        </div>
-      </router-link>
-
     </div>
   </div>
 </template>
