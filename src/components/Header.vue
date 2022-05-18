85c00cdb578aef0c38f76f02b6f69dbdea09e6a9
Dan Kim <itsdanya@gmail.com>
2022-05-19 01:36:52 +0300

    Optimise header for mobile

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="container py-8 flex justify-between items-center">
-    <div class="flex flex-row items-center gap-x-12">
+  <div class="container py-8 flex justify-between items-center gap-x-8">
+    <div class="flex flex-row items-center gap-x-8 md:gap-x-12">
       <router-link to="/">
         <img src="@/assets/images/logo.png" alt="Binary Blitz logo">
       </router-link>
